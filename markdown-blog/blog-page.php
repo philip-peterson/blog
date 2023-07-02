46f8785fa9aabb5ec7b2671c6ef124f7edff978d
Philip Peterson <philip.c.peterson@gmail.com>
2023-07-02 03:02:57 +0000

    Improve font

--- markdown-blog/blog-page.php
+++ markdown-blog/blog-page.php
@@ -17,7 +17,7 @@ if (file_exists($page)) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="blog.css">
+    <link rel="stylesheet" href="/blog.css">
     <title><?php echo $pageTitle ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
