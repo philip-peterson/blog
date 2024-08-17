a5cb59d9550180ac1d69670cce12677a6455575b
Philip Peterson <philip.c.peterson@gmail.com>
2024-08-17 04:38:13 -0400

    fix stuling

--- markdown-blog/blog-posts-list.php
+++ markdown-blog/blog-posts-list.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <title>QUINE Foundation Blog</title>
-    <link rel="stylesheet" href="blog.css">
+    <link rel="stylesheet" href="/blog.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
